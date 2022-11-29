1a8e3a3db06e5c36338d31f68a942f995051ee53
julia <jkroll2@mail.umw.edu>
2022-11-29 10:59:43 -0500

    BankAccount constructors

--- BankAccount.java
+++ BankAccount.java
@@ -1,2 +1,15 @@
 public class BankAccount {
+    private int account_number;
+    private int money;
+    private  AccountType type;
+    private int nextNumber = 1000000;
+
+    public BankAccount(){
+        account_number = nextNumber ;
+        nextNumber++;
+        money = 0;
+
+
+
+    }
 }
